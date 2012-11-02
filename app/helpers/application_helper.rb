100835ab4d8647293a6d84df31ab5d630e30a10b
denispeplin <denis.peplin@gmail.com>
2012-11-02 09:13:05 +0400

    Bugfixes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # A specific form builder to deal with filtering attributes out based on parameters
 class StrongParametersFormBuilder < SimpleForm::FormBuilder
   def input(attribute_name, options = {}, &block)
-    display_only = options[:display_only]
+    display_filter = options[:display_only]
 
     super unless display_filter && display_filter.include?(attribute_name)
   end
@@ -9,7 +9,7 @@ end
 
 # Form Helper to be used
 def strong_parameters_form_for(object, options = {}, &block)
-  simple_form_for(object, options.merge(:builder => StrongParametersFormBuilder)), &block)
+  simple_form_for(object, options.merge(:builder => StrongParametersFormBuilder), &block)
 end
 
 module ApplicationHelper
