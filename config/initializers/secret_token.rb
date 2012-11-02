e74b0eda3212e01000fc902e9dbd6e0beffac703
denispeplin <denis.peplin@gmail.com>
2012-11-02 08:43:46 +0400

    Rails new (3.2.8)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DisplayFilterDemo::Application.config.secret_token = '0609359f0740cc85e95a62d66ba9da049feb1ceb398162985b8c7d3d852f74e5d9cb408a179a4580e0916066424d90da5273c1d61ee1623862e94dde40836aa1'
